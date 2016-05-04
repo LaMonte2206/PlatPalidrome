743268e9c115e4ada7eefb7420751da8c28dc6ea
LaMonte <austinlamonte@yahoo.com>
2016-05-04 13:56:47 -0500

    still not working

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -5,13 +5,13 @@ import CheckViewControl from '../check/check.vc';
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
-    context: any = {
+    context = {
         
         userInput: '',
         error: ''
     };
     enter() {
-        let word = this.context.userInptu.trim(),
+        let word = this.context.userInput.trim(),
         wordLowercase = word.toLowerCase()
         if ( this.utils.isEmpty(word)) {
           this.context.error = "Please enter a word";
